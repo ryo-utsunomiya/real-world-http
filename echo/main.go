bead4665a55caa5f8aa7156ae4bea6b3cc18527e
Ryo Utsunomiya <millionsnowliving@gmail.com>
2019-12-21 12:08:09 +0900

    add digest handler

---
+++ echo/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/k0kubun/pp"
+)
+
+func main() {
+	const addr = ":18888"
+	log.Printf("listening %s", addr)
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/digest", handlerDigest)
+	server := http.Server{Addr: addr}
+	log.Fatal(server.ListenAndServe())
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+}
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %s\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %s\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="TgLc25U2BQA=f510a27804 73e18e6587be702c2e67fe2b04afd", algorithm=MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+}
